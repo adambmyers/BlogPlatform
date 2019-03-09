d41046e8345e429deb89cef4fb0308e1986a3b76
Parker Bartlett <prbartlett1@gmail.com>
2019-03-09 13:23:48 -0500

    Finished single post page and added dynamic home page

--- src/main/java/com/wecancodeit/blogsite/controllers/PostController.java
+++ src/main/java/com/wecancodeit/blogsite/controllers/PostController.java
@@ -5,18 +5,19 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.wecancodeit.blogsite.repositories.PostRepository;
 
 @Controller
-public class HomeController {
-	
+public class PostController {
+
 	@Resource
 	PostRepository posts;
 	
-	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("posts", posts.findAll());
-		return "home";
+	@GetMapping("/posts/{id}")
+	public String post(@PathVariable Long id, Model model) {
+		model.addAttribute("post", posts.findById(id).get());
+		return "post";
 	}
 }
